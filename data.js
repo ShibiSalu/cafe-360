cf0aa3f8316ec9fbcd2ee0bd1dd25c4853255108
ShibiSalu <shibisalu796@gmail.com>
2025-06-28 12:07:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,361 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cafe",
+      "name": "cafe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.03444542262097272,
+        "pitch": -0.07596238048176573,
+        "fov": 0.776789533382549
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32323627537106603,
+          "pitch": 0.0055191144619577415,
+          "rotation": 1.5707963267948966,
+          "target": "6-cafe-side"
+        },
+        {
+          "yaw": -0.04027545534258792,
+          "pitch": 0.015583872402443077,
+          "rotation": 0,
+          "target": "1-front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-front",
+      "name": "front",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.06611989167793375,
+        "pitch": 0.007497834495438838,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48068254846619496,
+          "pitch": -0.021693469074619998,
+          "rotation": 0,
+          "target": "2-counter-"
+        },
+        {
+          "yaw": -0.9353579057955237,
+          "pitch": 0.15592368825819491,
+          "rotation": 4.71238898038469,
+          "target": "4-wash-room"
+        },
+        {
+          "yaw": 1.353746349150338,
+          "pitch": -0.02923555975341685,
+          "rotation": 7.853981633974483,
+          "target": "5-out-side"
+        },
+        {
+          "yaw": 0.5789650322328335,
+          "pitch": 0.045787578032618015,
+          "rotation": 1.5707963267948966,
+          "target": "3-sitting"
+        },
+        {
+          "yaw": 2.570930620434498,
+          "pitch": 0.16387960886118869,
+          "rotation": 0,
+          "target": "0-cafe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-counter-",
+      "name": "counter ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.710671396088296,
+          "pitch": 0.14929083436814494,
+          "rotation": 0,
+          "target": "4-wash-room"
+        },
+        {
+          "yaw": 2.07291879555876,
+          "pitch": 0.12367252016561991,
+          "rotation": 0,
+          "target": "3-sitting"
+        },
+        {
+          "yaw": 2.837206392491175,
+          "pitch": 0.03317940696145705,
+          "rotation": 0,
+          "target": "0-cafe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sitting",
+      "name": "sitting",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.04299924003914768,
+        "pitch": -0.025701805930642863,
+        "fov": 0.8622129996295168
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6374832349866946,
+          "pitch": 0.09888726388517277,
+          "rotation": 0,
+          "target": "5-out-side"
+        },
+        {
+          "yaw": 1.4404138935070474,
+          "pitch": 0.16938360214474102,
+          "rotation": 0,
+          "target": "0-cafe"
+        },
+        {
+          "yaw": 3.045375062247362,
+          "pitch": 0.49287638502989317,
+          "rotation": 0,
+          "target": "2-counter-"
+        },
+        {
+          "yaw": 2.2350447735908903,
+          "pitch": -0.017700599080349377,
+          "rotation": 4.71238898038469,
+          "target": "4-wash-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-wash-room",
+      "name": "wash room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.483896851577331,
+        "pitch": -0.026187758300437736,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8746271836765906,
+          "pitch": 0.2572006796578279,
+          "rotation": 0,
+          "target": "1-front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-out-side",
+      "name": "out side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.038458731892072606,
+        "pitch": -0.08247617944984498,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06661237383534058,
+          "pitch": 0.11585981603402473,
+          "rotation": 0,
+          "target": "6-cafe-side"
+        },
+        {
+          "yaw": 1.5962261704639982,
+          "pitch": 0.0927906875020863,
+          "rotation": 1.5707963267948966,
+          "target": "1-front"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cafe-side",
+      "name": "cafe side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.08856853610235049,
+        "pitch": -0.012555402927413084,
+        "fov": 0.776789533382549
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3766496739646481,
+          "pitch": 0.13481389021490564,
+          "rotation": 4.71238898038469,
+          "target": "0-cafe"
+        },
+        {
+          "yaw": -0.02867692226353391,
+          "pitch": 0.18330034900556136,
+          "rotation": 7.853981633974483,
+          "target": "7-parking"
+        },
+        {
+          "yaw": -0.277586032195682,
+          "pitch": 0.05885548591527012,
+          "rotation": 0,
+          "target": "5-out-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-parking",
+      "name": "parking",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8732484909046043,
+          "pitch": 0.10797256035734648,
+          "rotation": 1.5707963267948966,
+          "target": "6-cafe-side"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "cafe",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
